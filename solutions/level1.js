d76cf758ccc7eebdb486eb94d5b4039d1f9be44c
JGamez17 <github email address>
2024-01-07 14:33:25 -0500

    WIP: Level1 solutions

---
+++ solutions/level1.js
@@ -0,0 +1,30 @@
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya',
+]
+
+const webTechs = [
+    'HTML',
+    'CSS',
+    'Javascript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB',
+]
+
+//1. Declare an empty array 
+const arr = Array() //creates an empty array 
+console.log(arr)
+
+//2. Declare an array with more than 5 number of elements 
+const fiveEmptyValues = Array(5)
